41defabd9fae76b372dfbabb791b60e149847852
Erick Banks <erick@puppetlabs.com>
2018-11-20 14:32:22 -0800

    run bundle install inside the container to pull in gems that work on linux

--- bundle_beaker.sh
+++ bundle_beaker.sh
@@ -1,3 +1,4 @@
 beaker-hostgenerator windows2008r2-64default.a --hypervisor vmpooler > /module/spec/acceptance/nodesets/temp.yaml 
 cd /module
+bundle install --path .bundle/gems
 bundle exec rake beaker
