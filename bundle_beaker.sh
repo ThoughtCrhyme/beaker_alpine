d3c7ca551f77193286fad027ca5e783f3a135ada
Erick Banks <erick@puppetlabs.com>
2018-11-20 15:12:42 -0800

    change default platform

--- bundle_beaker.sh
+++ bundle_beaker.sh
@@ -1,4 +1,4 @@
-beaker-hostgenerator windows2008r2-64default.a --hypervisor vmpooler > /module/spec/acceptance/nodesets/temp.yaml 
+beaker-hostgenerator windows2012r2-64default.a --hypervisor vmpooler > /module/spec/acceptance/nodesets/temp.yaml 
 cd /module
 bundle install --path .bundle/gems
 bundle exec rake beaker
